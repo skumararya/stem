356fed0aa62304fe6afce961dd9e37b6db4cb1f3
skumararya <skumararya.arya4@gmail.com>
2026-04-24 12:21:57 +0530

    update session 1 key result

--- Dashboard/student/videos/SESSION_1/story_content/user.js
+++ Dashboard/student/videos/SESSION_1/story_content/user.js
@@ -1,4 +1,4 @@
-/*
+
 window.InitUserScripts = function()
 {
 var player = GetPlayer();
@@ -6,32 +6,4 @@ var object = player.object;
 var addToTimeline = player.addToTimeline;
 var setVar = player.SetVar;
 var getVar = player.GetVar;
-};
-*/
-window.InitUserScripts = function () {
-
-  console.log("InitUserScripts fired");
-
-  setTimeout(() => {
-    const checkEnd = setInterval(() => {
-      try {
-        const currentSlide = document.querySelector(".slide-layer, .primary-slide");
-
-        if (currentSlide) {
-          const text = currentSlide.innerText || "";
-
-          if (/congratulations|completed|thank you|finished/i.test(text)) {
-            console.log("🎉 Course Completed");
-
-            window.parent.postMessage({
-              type: "COURSE_COMPLETED"
-            }, "*");
-
-            clearInterval(checkEnd);
-          }
-        }
-      } catch {}
-    }, 2000);
-  }, 5000);
-
 };
